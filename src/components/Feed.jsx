f311d33878ee229f20a3219ddd7f4e1d13e6a59c
badouine <nzbadouine@gmail.com>
2022-11-26 07:06:20 +0000

    sidebool

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,14 +1,17 @@
+import React, { useEffect, useState } from "react";
 import { Box, Stack, Typography } from "@mui/material";
 
-import { Sidebar } from "./";
+import { Videos, Sidebar } from "./";
 
 const Feed = () => {
- 
+  const [selectedCategory, setSelectedCategory] = useState("New");
+  const [videos, setVideos] = useState(null);
+
 
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
       <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 } }}>
-        <Sidebar />
+        <Sidebar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
         
         <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
           Copyright © 2022 JSM Media
@@ -17,10 +20,10 @@ const Feed = () => {
 
       <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
         <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
-           <span style={{ color: "#FC1503" }}>videos</span>
+          {selectedCategory} <span style={{ color: "#FC1503" }}>videos</span>
         </Typography>
 
-
+        <Videos videos={videos} />
       </Box>
     </Stack>
   );
